57501a46685c0174a61dc42d63ba37e11d28b802
MAIN\ntw9049 <ntw9049@g.rit.edu>
2017-01-20 20:21:25 -0500

    Random commits

---
+++ BatCops/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	Vector3 position;
+
+	public GameObject bat;
+	public float range;
+
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(DistanceSqr(bat.transform.position) > range*range){
+			MoveTowards();
+		}
+	}
+
+	void MoveTowards(){
+		float x = Mathf.Lerp(position.x,bat.transform.position.x,1f);
+		float z = Mathf.Lerp(position.z,bat.transform.position.z,1f);
+		Vector3 goal = new Vector3(x,position.y,z);
+	}
+
+	float DistanceSqr(Vector3 target){
+		return Mathf.Pow(position.z-target.z,2) + Mathf.Pow(position.z-target.z,2);
+	}
+}
---
+++ BatCops/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("up"))
+			transform.position = new Vector3(transform.position.x+speed,transform.position.y,transform.position.z);
+		if(Input.GetButton("down"))
+			transform.position = new Vector3(transform.position.x+speed,transform.position.y,transform.position.z);
+		if(Input.GetButton("left"))
+			transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+speed);
+		if(Input.GetButton("right"))
+			transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z-speed);
+	}
+}
