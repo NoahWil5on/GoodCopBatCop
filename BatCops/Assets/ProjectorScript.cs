d84c20b1ae91cdd905397960c522049cb3637187
Rose <cinorflynn@yahoo.com>
2017-01-20 20:51:52 -0500

    Added Instance Manager for the projector

--- BatCops/Assets/ProjectorScript.cs
+++ BatCops/Assets/ProjectorScript.cs
@@ -7,8 +7,33 @@ public class ProjectorScript : MonoBehaviour {
     bool up = true, powered = false;
     float poweredTime, poweredTimeStart = 0;
     Projector myProj;
-	// Use this for initialization
-	void Start () {
+
+    public static ProjectorScript instance = null;              
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+
+    }
+
+    
+    // Use this for initialization
+    void Start () {
         myProj = GetComponent<Projector>();
 	}
 	
